0aae9f1a7933f67ce2fe84d466305478b5b1b5ca
Jiří Vrba <jiri@vrba.dev>
2021-10-03 15:26:22 +0200

    Implement check for dead sci-hub links

--- src/js/scholar.js
+++ src/js/scholar.js
@@ -1,8 +1,8 @@
-const extractUrlFromLinkEvent = event => {
+const extractUrlFromLinkEvent = element => {
   // get its url
-  const target = event.target.tagName.toUpperCase() == "A" 
-    ? event.target
-    : event.target.parentElement;
+  const target = element.tagName.toUpperCase() == "A"
+    ? element
+    : element.parentElement;
 
   return target.getAttribute("href");
 }
@@ -19,14 +19,41 @@ const openLink = event => {
   event.preventDefault();
 
   // Get the link url
-  const url = extractUrlFromLinkEvent(event);
+  const link = event.target;
+  const url = extractUrlFromLinkEvent(link);
 
-  // open the link with configured sci-hub domain
-  withSciHubLink(url, link => window.open(link, "_blank"));
+  // If the link is marked as dead (on Sci-Hub),
+  // open the original link instead
+  if (link.classList.contains("__scihub--dead-link")) {
+    window.open(url, "_blank");
+  }
+  else {
+    // Otherswise, open the link with configured sci-hub domain
+    withSciHubLink(url, link => window.open(link, "_blank"));
+  }
 };
 
-const checkForDeadLink = event => {
-  console.log(event);
+const checkForDeadLink = element => {
+  // Get the link url + configured sci-hub proxy
+  const url = extractUrlFromLinkEvent(element);
+
+  withSciHubLink(url, link => {
+    // There is a requirement of using a CORS proxy (as Sci-Hub doesn't have appropriate CORS headers)
+    const proxy = "https://api.allorigins.win/get?url=";
+    const full = proxy + encodeURIComponent(link);
+  
+    fetch(full)
+      .then(response => response.text())
+      .then(body => {
+        // All links are valid, if the #article element is present
+        const valid = body.indexOf("#article") !== -1;
+
+        if (!valid) {
+          element.innerHTML += " <small style='color: #ff0000;'>Sci-Hub unavailable</small>";
+          element.classList.add("__scihub--dead-link");
+        }
+      })
+  });
 };
 
 (function () {
@@ -36,8 +63,8 @@ const checkForDeadLink = event => {
   // bind event listeners to them
   try {
     links.forEach((link) => {
-      link.addEventListener("mouseover", checkForDeadLink);
       link.addEventListener("click", openLink);
+      checkForDeadLink(link)
     });
   } catch (error) {
     console.warn(error);
